ec8af8bbc126a89895afb24f13355f9e1fcd3d3a
Rafael Nunes <rafaelnunes3092@gmail.com>
2019-05-03 10:43:35 +0100

    Updated repo

--- app/src/main/java/pt/rln/universe_explorer/GridViewPhotos.java
+++ app/src/main/java/pt/rln/universe_explorer/GridViewPhotos.java
@@ -1,16 +1,44 @@
 package pt.rln.universe_explorer;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.GridView;
 
+import java.util.ArrayList;
+
 public class GridViewPhotos extends AppCompatActivity {
+    public static final String TITLE = "Image#";
     private GridView gridView;
-//    private GridViewAdapter gridAdapter;
+    private GridViewAdapter gridAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gridph);
+
+        gridView = findViewById(R.id.gridViewph);
+        gridAdapter = new GridViewAdapter(this, R.layout.grid_item_layout, getData());
+        gridView.setAdapter(gridAdapter);
+    }
+
+    private ArrayList<ImageItem> getData(){
+        ArrayList<ImageItem> imageItems = new ArrayList<>();
+
+        TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
+
+        for (int i = 0; i < imgs.length(); i++) {
+            Resources res = getResources();
+            int id = imgs.getResourceId(i, -1);
+
+            Bitmap bitmap = BitmapFactory.decodeResource(res, id);
+
+            imageItems.add(new ImageItem(bitmap, TITLE +i));
+        }
+
+        return imageItems;
     }
 }
---
+++ app/src/main/java/pt/rln/universe_explorer/ImageItem.java
@@ -0,0 +1,29 @@
+package pt.rln.universe_explorer;
+
+import android.graphics.Bitmap;
+
+public class ImageItem {
+    private Bitmap image;
+    private String title;
+
+    public ImageItem(Bitmap image, String title){
+        this.image = image;
+        this.title = title;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
